9dd3956df899fc953ff26703dc8e9b9cffdddd18
Betul <betulkoru06@gmail.com>
2024-08-31 20:23:14 +0300

    feat: index struc built

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 'use strict'
+
+//* express
+
+// Required modules:
+
+//* dotenv var config
+
+//* async error handler
+
+// Configuration:
+
+//* Connect the Database
+
+// Middlewares:
+
+//* accept json, static uploadfile, auth check, logger, getModelList
+
+// Routes:
+
+// Error Handler:
+
+// Run Server:
+
+// Sync:
