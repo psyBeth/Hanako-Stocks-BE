c2b409b1dcccbd159c82b07157fb3e426ff366a3
Betul <betulkoru06@gmail.com>
2024-09-09 10:18:54 +0300

    feat: user schema struct

--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,42 @@
 'use strict'
 
 const { mongoose } = require('../configs/dbConnection');
+
+const UserSchema = new mongoose.Schema({
+
+    username: {
+
+    },
+
+    password: {
+
+    },
+
+    email: {
+
+    },
+
+    firstName: {
+
+    },
+
+    lastName: {
+
+    },
+
+    isActive: {
+
+    },
+
+    isStaff: {
+
+    },
+
+    isAdmin: {
+        
+    }
+
+}, {
+    collection: 'users',
+    timestamps: true
+})
