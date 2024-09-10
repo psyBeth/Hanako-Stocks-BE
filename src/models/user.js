257e970b2751ba652ead0f62b642dcc6da12491a
Betul <betulkoru06@gmail.com>
2024-09-10 12:50:27 +0300

    feat: user model

--- src/models/user.js
+++ src/models/user.js
@@ -5,38 +5,58 @@ const { mongoose } = require('../configs/dbConnection');
 const UserSchema = new mongoose.Schema({
 
     username: {
-
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true
     },
 
     password: {
-
+        type: String,
+        trim: true,
+        required: true
     },
 
     email: {
-
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        index: true,
+        // validate: ... (validation - pre(save))
     },
 
     firstName: {
-
+        type: String,
+        trim: true,
+        required: true
     },
 
     lastName: {
-
+        type: String,
+        trim: true,
+        required: true
     },
 
     isActive: {
-
+        type: Boolean,
+        default: true
     },
 
     isStaff: {
-
+        type: Boolean,
+        default: false
     },
 
     isAdmin: {
-        
+        type: Boolean,
+        default: false   
     }
 
 }, {
     collection: 'users',
     timestamps: true
 })
+
+module.exports = mongoose.model('User', UserSchema);
