c943133f05a0d07798d376a6776ef23a59728a55
Betul <betulkoru06@gmail.com>
2024-09-11 13:18:45 +0300

    feat: password encrypt added user model

--- src/models/user.js
+++ src/models/user.js
@@ -51,7 +51,7 @@ const UserSchema = new mongoose.Schema({
 
     isAdmin: {
         type: Boolean,
-        default: false   
+        default: false
     }
 
 }, {
@@ -59,6 +59,60 @@ const UserSchema = new mongoose.Schema({
     timestamps: true
 })
 
-//! Password encrypt will be used here 
+// https://mongoosejs.com/docs/middleware.html
+
+const passwordEncrypt = require('../helpers/passwordEncrypt');
+
+UserSchema.pre(['save', 'updateOne'], function (next) {
+    // console.log('pre(save,update) run.')
+    // console.log(this)
+
+    // get data from "this" or "this._update"
+    const data = this?._update || this
+
+    // email@domain.com
+    const isEmailValidated = data.email ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email) : true
+
+    if (isEmailValidated) {
+
+        // console.log('Email OK')
+
+        if (data?.password) {
+
+            const isPasswordValidated = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(data.password)
+
+            if (isPasswordValidated) {
+
+                // console.log('Password OK')
+
+                data.password = passwordEncrypt(data.password)
+
+                if (this?._update) {
+
+                    this._update = data
+                    // this._update.password = data.password 
+
+                } else {
+                    // this = data // doesn't allow
+                    this.password = data.password
+                }
+
+                //? ShortHand:
+                // // save:
+                // this.password = data.password = passwordEncrypt(data.password)
+                // // update:
+                // this._update = data
+
+
+            } else {
+                next(new Error('Password is not validated.'))
+            }
+        }
+        next()
+
+    } else {
+        next(new Error('Email is not validated.'))
+    }
+})
 
 module.exports = mongoose.model('User', UserSchema);
