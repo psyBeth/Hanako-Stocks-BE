f02117144ee129e44b2e0796d7ecf91297b734a6
Betul <betulkoru06@gmail.com>
2024-09-13 10:49:48 +0300

    feat: token model

--- src/models/token.js
+++ src/models/token.js
@@ -13,9 +13,21 @@ const TokenSchema = new mongoose.Schema({
 
   userId: {
     type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    index: true
+  },
+
+  token: {
+    type: String,
+    trim: true,
+    required: true,
+    index: true
   }
 
 }, {
   collection: 'tokens',
   timestamps: true
 });
+
+module.exports = mongoose.model('Token', TokenSchema);
