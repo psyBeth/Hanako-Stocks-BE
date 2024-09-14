07f2723efb8247e26229975e7c84d77892e12cd1
Betul <betulkoru06@gmail.com>
2024-09-14 18:52:13 +0300

    feat: user ctrllr intro

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,3 +1,28 @@
 'use strict'
 
 const User = require('../models/user');
+const Token = require('../models/token');
+const passwordEncrypt = require('../helpers/passwordEncrypt');
+
+module.exports = {
+
+    list: async (req, res) => {
+
+    },
+
+    create: async (req, res) => {
+
+    },
+
+    read: async (req, res) => {
+
+    },
+
+    update: async (req, res) => {
+
+    },
+
+    delete: async (req, res) => {
+
+    }
+}
