333e9eac51168e13e2b625b84630cf87fbec73b8
Betul <betulkoru06@gmail.com>
2024-09-15 23:51:52 +0300

    feat: swaggers added user ctrl

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -7,22 +7,68 @@ const passwordEncrypt = require('../helpers/passwordEncrypt');
 module.exports = {
 
     list: async (req, res) => {
-
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "List Users"
+            #swagger.description = `
+                You can use <u>filter[] & search[] & sort[] & page & limit</u> queries with endpoint.
+                <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=asc&sort[field2]=desc</b></li>
+                    <li>URL/?<b>limit=10&page=1</b></li>
+                </ul>
+            `
+        */
     },
 
     create: async (req, res) => {
-
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Create User"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "1234",
+                    "email": "test@site.com",
+                    "firstName": "test",
+                    "lastName": "test",
+                }
+            }
+        */
     },
 
     read: async (req, res) => {
-
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Get Single User"
+        */
     },
 
     update: async (req, res) => {
-
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Update User"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "1234",
+                    "email": "test@site.com",
+                    "firstName": "test",
+                    "lastName": "test",
+                }
+            }
+        */
     },
 
     delete: async (req, res) => {
-
+        /*
+            #swagger.tags = ["Users"]
+            #swagger.summary = "Delete User"
+        */
     }
 }
