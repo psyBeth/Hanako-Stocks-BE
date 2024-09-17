d444497599fc39ea611e385bff13515fdf23663a
Betul <betulkoru06@gmail.com>
2024-09-17 21:58:33 +0300

    feat: auth ctrl elements

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,3 +5,18 @@ const User = require('../models/user');
 const Token = require('../models/token');
 const passwordEncrypt = require('../helpers/passwordEncrypt');
 
+module.exports = {
+
+    login: async (req, res) => {
+
+    },
+
+    refresh: async (req, res) => {
+
+    },
+
+    logout: async (req, res) => {
+        
+    }
+
+}
