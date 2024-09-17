b6f2028cee38fe29d30592f579fefc82e335122f
Betul <betulkoru06@gmail.com>
2024-09-17 21:56:37 +0300

    feat: auth ctrl required modules

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,2 +1,7 @@
 'use strict'
 
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const Token = require('../models/token');
+const passwordEncrypt = require('../helpers/passwordEncrypt');
+
