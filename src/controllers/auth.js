bbc92dab92383dca67990146b8181b203f616645
Betul <betulkoru06@gmail.com>
2024-09-17 21:59:24 +0300

    feat: auth ctrl swaggers

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -8,15 +8,44 @@ const passwordEncrypt = require('../helpers/passwordEncrypt');
 module.exports = {
 
     login: async (req, res) => {
-
+        /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "Login"
+            #swagger.description = 'Login with username (or email) and password for get Token and JWT.'
+            #swagger.parameters["body"] = {
+                in: "body",
+                required: true,
+                schema: {
+                    "username": "test",
+                    "password": "1234",
+                }
+            }
+        */
     },
 
     refresh: async (req, res) => {
-
+        /*
+            #swagger.tags = ['Authentication']
+            #swagger.summary = 'JWT: Refresh'
+            #swagger.description = 'Refresh access-token by refresh-token.'
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    bearer: {
+                        refresh: '___refreshToken___'
+                    }
+                }
+            }
+        */
     },
 
     logout: async (req, res) => {
-        
+        /*
+            #swagger.tags = ["Authentication"]
+            #swagger.summary = "Token: Logout"
+            #swagger.description = 'Delete token-key.'
+        */
     }
 
 }
