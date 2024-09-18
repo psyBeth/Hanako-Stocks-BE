f0aaec4fb42cc3ddc9d9c263c99ae9fca86218d9
Betul <betulkoru06@gmail.com>
2024-09-18 10:35:46 +0300

    feat: auth ctrl jwt login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -21,6 +21,53 @@ module.exports = {
                 }
             }
         */
+
+        const { username, email, password } = req.body;
+
+        if ((username || email) && password) {
+
+            const user = await User.findOne({ $or: [{ username }, { email }] });
+
+            if (user && user.password == passwordEncrypt(password)) {
+
+                if (user.isActive) {
+                    // Use UUID:
+                    // const { randomUUID } = require('crypto')
+                    // let tokenData = await Token.findOne({ userId: user._id })
+                    // if (!tokenData) tokenData = await Token.create({
+                    //     userId: user._id,
+                    //     token: randomUUID()
+                    // })
+
+                    // TOKEN:
+                    let tokenData = await Token.findOne({ userId: user._id });
+                    if (!tokenData) tokenData = await Token.create({
+                        userId: user._id,
+                        token: passwordEncrypt(user._id + Date.now())
+                    });
+
+                    // JWT:
+                    const accessToken = jwt.sign(user.toJSON(), process.env.ACCESS_KEY, { expiresIn: '30m' });
+                    const refreshToken = jwt.sign({ _id: user._id, password: user.password }, process.env.REFRESH_KEY, { expiresIn: '3d' });
+
+                    res.send({
+                        error: false,
+                        token: tokenData.token,
+                        bearer: { accessToken, refreshToken },
+                        user
+                    });
+                } else {
+                    res.errorStatusCode = 401;
+                    throw new Error('This account is not active.');
+                }
+            } else {
+                res.errorStatusCode = 401;
+                throw new Error('Wrong username/email or password.');
+            }
+        } else {
+            res.errorStatusCode = 401;
+            throw new Error('Please enter username/email and password.');
+        }
     },
 
     refresh: async (req, res) => {
