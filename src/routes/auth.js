2b9ea8202dc461502ae8887bf96101c15ac83a99
Betul <betulkoru06@gmail.com>
2024-09-18 12:52:23 +0300

    feat: auth router

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,3 +1,13 @@
 'use strict'
 
 const router = require('express').Router();
+
+const auth = require('../controllers/auth');
+
+// URL: /auth
+
+router.post('/login', auth.login); // Simple Token & JWT
+router.post('/refresh', auth.refresh); // JWT refresh
+router.get('/logout', auth.logout); // Simple Token Logout
+
+module.exports = router;
