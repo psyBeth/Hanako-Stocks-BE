84a117481c8066bc0c9c2c2523ac39826d9ef00e
Betul <betulkoru06@gmail.com>
2024-09-19 15:00:57 +0300

    feat: user model mock data added fr later

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,39 @@
 
 const { mongoose } = require('../configs/dbConnection');
 
+/* ------------------------------------------------------- *
+{
+    "username": "admin",
+    "password": "aA?123456",
+    "email": "admin@site.com",
+    "firstName": "admin",
+    "lastName": "admin",
+    "isActive": true,
+    "isStaff": true,
+    "isAdmin": true
+}
+{
+    "username": "staff",
+    "password": "aA?123456",
+    "email": "staff@site.com",
+    "firstName": "staff",
+    "lastName": "staff",
+    "isActive": true,
+    "isStaff": true,
+    "isAdmin": false
+}
+{
+    "username": "test",
+    "password": "aA?123456",
+    "email": "test@site.com",
+    "firstName": "test",
+    "lastName": "test",
+    "isActive": true,
+    "isStaff": false,
+    "isAdmin": false
+}
+/* ------------------------------------------------------- */
+
 const UserSchema = new mongoose.Schema({
 
     username: {
