4c0782deed79471751f93e3239bf2c73c27dbb5c
Betul <betulkoru06@gmail.com>
2024-09-22 22:06:38 +0300

    feat: sales model intro

--- src/models/sale.js
+++ src/models/sale.js
@@ -1,1 +1,28 @@
 'use strict'
+
+const { mongoose } = require('../configs/dbConnection');
+
+const SaleSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    brandId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Brand",
+        required: true
+    },
+
+    productId: {
+        //! product first ig
+    }
+
+}, {
+    collection: "sales",
+    timestamps: true
+});
+
+module.exports = mongoose.model("Sale", SaleSchema);
