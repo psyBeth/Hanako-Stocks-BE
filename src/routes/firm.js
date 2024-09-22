01c83788ade6483496381788fa8f5e567d445fa7
Betul <betulkoru06@gmail.com>
2024-09-22 21:53:03 +0300

    feat: firms routing

---
+++ src/routes/firm.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const router = require('express').Router();
+
+const firm = require('../controllers/firm');
+const permissions = require('../middlewares/permissions');
+
+// URL: /firms
+
+router.route('/(:id)?')
+    .post(permissions.isAdmin, firm.create)
+    .get(permissions.isStaff, firm.read)
+    .put(permissions.isAdmin, firm.update)
+    .patch(permissions.isAdmin, firm.update)
+    .delete(permissions.isAdmin, firm.delete);
+
+module.exports = router;
