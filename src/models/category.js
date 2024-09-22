79fbf86b875f1309e66c3413d6a0ac83d2c60fb5
Betul <betulkoru06@gmail.com>
2024-09-22 15:33:17 +0300

    feat: category model

--- src/models/category.js
+++ src/models/category.js
@@ -1,1 +1,20 @@
 'use strict'
+
+const { mongoose } = require('../configs/dbConnection');
+
+const CategorySchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true,
+        set: name => name.toUpperCase()
+    }
+
+}, {
+    collection: 'categories',
+    timestamps: true
+});
+
+module.exports = mongoose.model('Category', CategorySchema);
