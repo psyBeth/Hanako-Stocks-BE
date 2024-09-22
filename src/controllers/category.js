d1153570daa79e75d154843a4cdf55fa610b090f
Betul <betulkoru06@gmail.com>
2024-09-22 15:35:56 +0300

    feat: category controller intro

---
+++ src/controllers/category.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const Category = require('../models/category');
+
+module.exports = {
+
+    list: async (req, res) => {
+
+    },
+
+    create: async (req, res) => {
+
+    },
+
+    read: async (req, res) => {
+
+    },
+
+    update: async (req, res) => {
+
+    },
+
+    delete: async (req, res) => {
+
+    }
+
+};
