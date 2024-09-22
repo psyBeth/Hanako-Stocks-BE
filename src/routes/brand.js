7955bfcd1569594efdfe20997c9f0518b426edb7
Betul <betulkoru06@gmail.com>
2024-09-22 21:11:52 +0300

    feat: brand routing

---
+++ src/routes/brand.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const router = require('express').Router();
+
+const brand = require('../controllers/brand');
+const permissions = require('../middlewares/permissions');
+
+// URL: /brands
+
+router.route('/(:id)?')
+    .post(permissions.isAdmin, brand.create)
+    .get(permissions.isStaff, brand.read)
+    .put(permissions.isAdmin, brand.update)
+    .patch(permissions.isAdmin, brand.update)
+    .delete(permissions.isAdmin, brand.delete);
+
+module.exports = router;
