8f920232acb3ddfe722f639ee9e09ad5bc1c1fb5
Betul <betulkoru06@gmail.com>
2024-09-22 22:13:45 +0300

    feat: product model

--- src/models/product.js
+++ src/models/product.js
@@ -1,1 +1,40 @@
 'use strict'
+
+const { mongoose } = require('../configs/dbConnection');
+
+const ProductSchema = new mongoose.Schema({
+
+    categoryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Category",
+        required: true
+    },
+
+    brandId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Brand",
+        required: true
+    },
+
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+
+    quantity: {
+        type: Number,
+        default: 0
+    },
+
+    price: {
+        type: Number,
+        default: 0
+    }
+
+}, {
+    collection: "products",
+    timestamps: true
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
