b3cda1ab696a0bec50c57339a9cf3c74448e75a4
Betul <betulkoru06@gmail.com>
2024-09-22 20:54:48 +0300

    feat: brand model

--- src/models/brand.js
+++ src/models/brand.js
@@ -1,1 +1,24 @@
 'use strict'
+
+const { mongoose } = require('../configs/dbConnection');
+
+const BrandSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true
+    },
+
+    image: {
+        type: String,
+        trim: true
+    }
+
+}, {
+    collection: "brands",
+    timestamps: true
+});
+
+module.exports = mongoose.model("Brand", BrandSchema);
