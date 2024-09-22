3ad8da83e04b5f711cc8adb19ad209adc7dff1f1
Betul <betulkoru06@gmail.com>
2024-09-22 21:20:31 +0300

    feat: firm model

--- src/models/firm.js
+++ src/models/firm.js
@@ -1,1 +1,28 @@
 'use strict'
+
+const { mongoose } = require('../configs/dbConnection');
+
+const FirmSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true
+    },
+
+    phone: String,
+
+    adress: String,
+
+    image: {
+        type: String,
+        trim: true
+    }
+
+}, {
+    collection: "firms",
+    timestamps: true
+});
+
+module.exports = mongoose.model("Firm", FirmSchema);
