7218dc196cd78cd842f40c5c32bb495b06a879e3
Betul <betulkoru06@gmail.com>
2024-09-22 21:07:56 +0300

    feat: brand controller

--- src/controllers/brand.js
+++ src/controllers/brand.js
@@ -18,6 +18,14 @@ module.exports = {
                 </ul>
             `
         */
+
+        const data = await res.getModelList(Brand);
+
+        res.status(200).send({
+            error: false,
+            details: await res.getModelListDetails(Brand),
+            data
+        });
     },
 
     create: async(req, res) => {
@@ -32,6 +40,13 @@ module.exports = {
                 }
             }
         */
+
+        const data = await Brand.create(req.body);
+
+        res.status(201).send({
+            error: false,
+            data
+        });
     },
 
     read: async(req, res) => {
@@ -39,6 +54,31 @@ module.exports = {
             #swagger.tags = ["Brands"]
             #swagger.summary = "Get Single Brand"
         */
+
+        // console.log();
+
+        if(req.params?.id) {
+
+            // Single:
+            const data = await Brand.findOne({_id: req.params.id});
+
+            res.status(200).send({
+                error: false,
+                data
+            });
+
+        } else {
+
+            // All:
+            const data = await res.getModelList(Brand);
+
+            res.status(200).send({
+                error: false,
+                details: await res.getModelListDetails(Brand),
+                data
+            });
+
+        };
     },
 
     update: async(req, res) => {
@@ -53,6 +93,14 @@ module.exports = {
                 }
             }
         */
+
+        const data = await Brand.updateOne({_id: req.params.id}, req.body, {runValidators: true});
+
+        res.status(202).send({
+            error: false,
+            data,
+            new: await Brand.findOne({_id: req.params.id})
+        });
     },
 
     delete: async(req, res) => {
@@ -60,6 +108,13 @@ module.exports = {
             #swagger.tags = ["Brands"]
             #swagger.summary = "Delete Brand"
         */
+
+        const data = await Brand.deleteOne({_id: req.params.id});
+
+        res.status(data.deletedCount ? 204 : 404).send({
+            error: !data.deletedCount,
+            data
+        });
     }
 
 };
