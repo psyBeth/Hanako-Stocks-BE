9835954d92c62a86f2b644fea8af1156f8ec3eca
Betul <betulkoru06@gmail.com>
2024-09-22 20:30:05 +0300

    feat: category ctrl swaggers

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -5,23 +5,61 @@ const Category = require('../models/category');
 module.exports = {
 
     list: async (req, res) => {
-
+        /*
+            #swagger.tags = ["Categories"]
+            #swagger.summary = "List Categories"
+            #swagger.description = `
+                You can use <u>filter[] & search[] & sort[] & page & limit</u> queries with endpoint.
+                <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=asc&sort[field2]=desc</b></li>
+                    <li>URL/?<b>limit=10&page=1</b></li>
+                </ul>
+            `
+        */       
     },
 
     create: async (req, res) => {
-
+        /*
+            #swagger.tags = ["Categories"]
+            #swagger.summary = "Create Category"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    "name": "Category 1"
+                }
+            }
+        */
     },
 
     read: async (req, res) => {
-
+        /*
+            #swagger.tags = ["Categories"]
+            #swagger.summary = "Get Single Category"
+        */
     },
 
     update: async (req, res) => {
-
+        /*
+            #swagger.tags = ["Categories"]
+            #swagger.summary = "Update Category"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    "name": "Category 1"
+                }
+            }
+        */
     },
 
     delete: async (req, res) => {
-
+        /*
+            #swagger.tags = ["Categories"]
+            #swagger.summary = "Delete Category"
+        */
     }
 
 };
