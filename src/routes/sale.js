e8f6a28983b3355ec0c1dae5fc124564d7d806e0
Betul <betulkoru06@gmail.com>
2024-09-23 16:00:31 +0300

    feat: sales routing

---
+++ src/routes/sale.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const router = require('express').Router();
+
+const sale = require('../controllers/sale');
+const permissions = require('../middlewares/permissions');
+
+// URL: /sales
+
+router.route('/(:id)?')
+    .post(permissions.isAdmin, sale.create)
+    .get(permissions.isStaff, sale.read)
+    .put(permissions.isAdmin, sale.update)
+    .patch(permissions.isAdmin, sale.update)
+    .delete(permissions.isAdmin, sale.delete);
+
+module.exports = router;
