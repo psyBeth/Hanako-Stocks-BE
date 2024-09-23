0a0d2ff4a73e981f51ef2e7fe3da46cdfceda97e
Betul <betulkoru06@gmail.com>
2024-09-23 15:51:28 +0300

    feat: sale controller

--- src/controllers/sale.js
+++ src/controllers/sale.js
@@ -5,7 +5,7 @@ const Product = require('../models/product');
 
 module.exports = {
 
-    list: async(req, res) => {
+    list: async (req, res) => {
         /*
             #swagger.tags = ["Sales"]
             #swagger.summary = "List Sales"
@@ -19,9 +19,21 @@ module.exports = {
                 </ul>
             `
         */
+
+        const data = await re.getModelList(Sale, {}, [
+            { path: 'userId', select: 'username email' },
+            'brandId',
+            { path: 'productId', select: 'name', populate: { path: 'categoryId' } }
+        ]);   // populating ref.
+
+        res.status(200).send({
+            error: false,
+            details: await res.getModelListDetails(Sale),
+            data
+        });
     },
 
-    create: async(req, res) => {
+    create: async (req, res) => {
         /*
             #swagger.tags = ["Sales"]
             #swagger.summary = "Create Sale"
@@ -33,16 +45,64 @@ module.exports = {
                 }
             }
         */
+
+        // get userId data from req.body
+        req.body.userId = req.user._id;
+
+        // get info abt the current product
+        const currentProduct = await Product.findOne({ _id: req.body.productId });
+
+        if (currentProduct.quantity >= req.body.quantity) {
+
+        } else {
+            res.errorStatusCode = 422;
+            throw new Error('There is not enough product in the stocks for this sale quantity.', {
+                cause: { currentProduct }
+            });
+        };
     },
 
-    read: async(req, res) => {
+    read: async (req, res) => {
         /*
             #swagger.tags = ["Sales"]
             #swagger.summary = "Get Single Sale"
         */
+
+        // console.log();
+
+        if (req.params?.id) {
+
+            // Single:
+            const data = await Sale.findOne({ _id: req.params.id }).populate([
+                { path: 'userId', select: 'username email' },
+                'brandId',
+                { path: 'productId', select: 'name', populate: { path: 'categoryId' } }
+            ]);
+
+            res.status(200).send({
+                error: false,
+                data
+            });
+
+        } else {
+
+            // All: 
+            const data = await re.getModelList(Sale, {}, [
+                { path: 'userId', select: 'username email' },
+                'brandId',
+                { path: 'productId', select: 'name', populate: { path: 'categoryId' } }
+            ]);
+
+            res.status(200).send({
+                error: false,
+                details: await res.getModelListDetails(Sale),
+                data
+            });
+
+        }
     },
 
-    update: async(req, res) => {
+    update: async (req, res) => {
         /*
             #swagger.tags = ["Sales"]
             #swagger.summary = "Update Sale"
@@ -54,13 +114,50 @@ module.exports = {
                 }
             }
         */
+
+        if (req.body?.quantity) {
+            // get info abt the current sale
+            const currentSale = await Sale.findOne({ _id: req.body.productId });
+            // amount difference
+            const difference = req.body.quantity - currentSale.quantity;
+            // save difference to product
+            const updateProduct = await Product.updateOne({ _id: currentSale.productId, quantity: { $gte: difference } }, { $inc: { quantity: -difference } });
+            // console.log(updateProduct);
+
+            // if error in update, stop the process
+            if (updateProduct.modifiedCount == 0) {
+                res.errorStatusCode = 422;
+                throw new Error('There is an issue updating the product quantity.');
+            };
+        };
+
+        const data = await Sale.updateOne({ _id: req.params.id }, req.body, { runValidators: true });
+
+        res.status(202).send({
+            error: false,
+            data,
+            new: await Sale.findOne({ _id: req.params.id })
+        });
     },
 
-    delete: async(req, res) => {
+    delete: async (req, res) => {
         /*
             #swagger.tags = ["Sales"]
             #swagger.summary = "Delete Sale"
         */
+
+        // get info abt the current sale
+        const currentSale = await Sale.findOne({ _id: req.body.productId });
+
+        const data = await Sale.deleteOne({_id: req.params.id});
+
+        // increase amount in product //!
+        const updateProduct = await Product.updateOne({ _id: currentSale.productId }, { $inc: { quantity: +currentSale.quantity } });
+
+        res.status(data.deletedCount ? 204 : 404).send({
+            error: !data.deletedCount,
+            data
+        });
     }
 
 };
