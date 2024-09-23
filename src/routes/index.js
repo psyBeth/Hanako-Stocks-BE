d0c40f69683f2ba8e8f375d16568665fa66c5b80
Betul <betulkoru06@gmail.com>
2024-09-23 16:01:25 +0300

    chore: clean code

--- src/routes/index.js
+++ src/routes/index.js
@@ -21,9 +21,9 @@ router.use('/firms', require('./firm'));
 // product:
 router.use('/products', require('./product'));
 // // purchase:
-// router.use('/purchases', require('./purchase'))
+// router.use('/purchases', require('./purchase'));
 // sale:
-router.use('/sales', require('./sale'))
+router.use('/sales', require('./sale'));
 
 // document:
 router.use('./documents', require('./document'));
