3bf08d20f147732bc664dceacf34a0e2e071b5e0
Betul <betulkoru06@gmail.com>
2024-09-23 18:31:57 +0300

    feat: purchase routing

---
+++ src/routes/purchase.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const router = require('express').Router();
+
+const purchase = require('../controllers/purchase');
+const permissions = require('../middlewares/permissions');
+
+// URL: /purchases
+
+router.route('/(:id)?')
+    .post(permissions.isAdmin, purchase.create)
+    .get(permissions.isStaff, purchase.read)
+    .put(permissions.isAdmin, purchase.update)
+    .patch(permissions.isAdmin, purchase.update)
+    .delete(permissions.isAdmin, purchase.delete);
+
+module.exports = router;
